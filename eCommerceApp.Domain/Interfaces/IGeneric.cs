09dcbead85a43b52280ca2dea7faf96c54e25701
Lucas Costa <lucasjcosta.98@gmail.com>
2024-11-12 14:44:51 -0300

    Add project files.

---
+++ eCommerceApp.Domain/Interfaces/IGeneric.cs
@@ -0,0 +1,11 @@
+﻿namespace eCommerceApp.Domain.Interfaces
+{
+    public interface IGeneric<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(Guid id);
+        Task<int> AddAsync(TEntity entity);
+        Task<int> UpdateAsync(Guid entity);
+        Task<int> DeleteAsync(Guid id);
+    }
+}
